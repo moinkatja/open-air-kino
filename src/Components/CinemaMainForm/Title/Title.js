d0f2f7c71b18bd17da987cb73270c5c9919345eb
kopikob <55636811+kopikob@users.noreply.github.com>
2020-09-29 15:52:27 +0200

    GitHub icon added, unused packages removed

--- src/Components/CinemaMainForm/Title/Title.js
+++ src/Components/CinemaMainForm/Title/Title.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from "../../../img/logo.png";
 import classes from "./Title.module.css";
-
+import { Icon  } from "@iconify/react";
+import scGithub from "@iconify/icons-ei/sc-github";
 
 function Title() {
     return (
 
         <header className={classes.Header}>
-            {/* <Link to="/"> */}
-                <img className={classes.Logo} src={logo} alt="Logo" />
-                <h1 className={classes.Title}>OpenAir Kino </h1>
-            {/* </Link> */}
+            <img className={classes.Logo} src={logo} alt="Logo" />
+            <h1 className={classes.Title}>OpenAir Kino </h1>
+            <a className={classes.GitHubLink}
+                href="https://github.com/moinkatja/open-air-kino"
+                title="GitHub Project">
+                <Icon icon={scGithub} color="darkgrey" width="50" height="50" />
+            </a>
         </header>
 
     )
