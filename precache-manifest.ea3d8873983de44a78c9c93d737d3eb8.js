265be7d62aa5093b7bfbb4e3e1ab35ec34f752c9
kopikob <55636811+kopikob@users.noreply.github.com>
2020-10-08 11:43:09 +0200

    Updates

---
+++ precache-manifest.ea3d8873983de44a78c9c93d737d3eb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0135be6ef4c18289cd79db20386bf4d6",
+    "url": "/open-air-kino/index.html"
+  },
+  {
+    "revision": "880ec94cf4772715a58d",
+    "url": "/open-air-kino/static/css/main.bfcaf8cb.chunk.css"
+  },
+  {
+    "revision": "315717ab8e022d6b6e64",
+    "url": "/open-air-kino/static/js/2.162c893c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/open-air-kino/static/js/2.162c893c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880ec94cf4772715a58d",
+    "url": "/open-air-kino/static/js/main.718309d9.chunk.js"
+  },
+  {
+    "revision": "4ccadcbeb22d492e88de",
+    "url": "/open-air-kino/static/js/runtime-main.4d1e2005.js"
+  },
+  {
+    "revision": "42465451f0c917ff08f43f12922de82f",
+    "url": "/open-air-kino/static/media/cinema.42465451.jpg"
+  },
+  {
+    "revision": "0449447ff11f02e92b8c262b2aa96a42",
+    "url": "/open-air-kino/static/media/popcorn.0449447f.png"
+  }
+]);
